44d0c72f8adac62d507da1426ea6584419418fba
Siddarath3670 <109130972+Siddarath3670@users.noreply.github.com>
2023-12-17 17:13:01 +0530

    Create index.js

---
+++ plaindrome-checker/index.js
@@ -0,0 +1,7 @@
+function palindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  // Compare the original cleaned string with its reverse
+  return cleanedStr === cleanedStr.split('').reverse().join('');
+}
